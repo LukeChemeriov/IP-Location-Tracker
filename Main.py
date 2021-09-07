d17e71007e798bef9c7924d38dff4327f2780104
СтранныйРусский <87149262+LukeChemeriov@users.noreply.github.com>
2021-09-07 13:22:05 +0100

    Update Main.py
    Initial commit

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
+# IMPORTS
+from geolite2 import geolite2
+import requests
 
+
+# thanks to W3W3W3
+
+#main function
+def my_ip_location(my_ip):
+    reader = geolite2.reader()
+    location = reader.get(my_ip)
+
+    # geolite database dict values and fine tunning
+    a=(location['city']['names']['en'])
+    b=(location['continent']['names']['en'])
+    c=(location['country']['names']['en'])
+    d=(location['location'])
+    e=(location['postal'])
+    f=(location['registered_country']['names']['en'])
+    g=(location['subdivisions'][0]['names']['en'])
+
+    print('''city: %s\ncontinent: %s\ncountry: %s\nlocation: %s\npostal: %s\nregistered_country: %s\nsubdivisions: %s\n'''
+     % (a,b,c,d,e,f,g))
+
+print('Enter IP')
+ip = input()
+
+my_ip_location(ip)
